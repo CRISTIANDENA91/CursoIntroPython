454f90c6ab63764f83697205d820dac9581be39e
CRISTIAN IRVING DENA BELTRAN <77164502+CRISTIANDENA91@users.noreply.github.com>
2022-02-18 18:20:07 -0600

    Manejo de Errores
    Este tema no lo comprendí del todo, al inicio me salían los errores, pero avanzando a los  ejercicios mostraba errores de los cuales no eran los esperados . Dejo algunas capturas, el código se modificó muchas veces.

---
+++ MODULO10/config2.py
@@ -0,0 +1,7 @@
+def main():
+    try:
+        configuration = open()
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
